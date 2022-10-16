a55819c8788e2988a6a1a81bb8a753b72523acf1
JamesB <James.buchmann@gmail.com>
2022-10-16 16:28:31 -0500

    added custom validator to user and disc bag size

--- my-app/server/models/user.js
+++ my-app/server/models/user.js
@@ -1,61 +1,68 @@
-const mongoose = require('mongoose')
-bcrypt = require('bcrypt')
+const mongoose = require("mongoose");
+bcrypt = require("bcrypt");
 
 SALT_FACTORY = 10;
 
 const UserSchema = new mongoose.Schema({
-    name: {
+  name: {
+    type: String,
+    default: null,
+    // required: true
+  },
+  password: {
+    type: String,
+    // required: true
+  },
+  discBag: { 
+      type: [
+      
+    {
+        
+      manufacturer: {
         type: String,
-        default: null
-        // required: true
-    },
-    password: {
+
+      },
+      name: {
         type: String,
-        // required: true
+        
+      },
+
     },
-    discBag: [{
-        manufacturer: {
-            type: String
-        },
-        name: {
-            type: String
-        }
-    }], 
-    token: { type: String},
-
-})
-
-
+    
+  ],
+  validate: {
+    validator: function(v,x,y) {
+        return !(this.discBag.length > 3);
+    },
+    message: props => `${props.value} exceeds maximum array size of 15`
+} },
+  token: { type: String },
+});
 
 UserSchema.pre("save", function (next) {
-    const user = this
-    console.log(user)
-        // only hashes passwords that are new or being modified.
-    if (this.isModified("password") || this.isNew) {
-        bcrypt.genSalt(10, function(saltError, salt) {
-            if (saltError) {
-                return next(saltError)
-            } else {
-                bcrypt.hash(user.password, salt, function(hashError, hash) {
-                    if (hashError) {
-                        return next( hashError )
-                    }
+  const user = this;
+  console.log(user);
+  // only hashes passwords that are new or being modified.
+  if (this.isModified("password") || this.isNew) {
+    bcrypt.genSalt(10, function (saltError, salt) {
+      if (saltError) {
+        return next(saltError);
+      } else {
+        bcrypt.hash(user.password, salt, function (hashError, hash) {
+          if (hashError) {
+            return next(hashError);
+          }
 
+          user.password = hash;
+          console.log(hash);
+          console.log(user.password);
+          next();
+        });
+      }
+    });
+  } else {
+    return next();
+  }
+});
 
-                    user.password = hash
-                    console.log(hash)
-                    console.log(user.password)
-                    next()
-                })
-            }
-        })
-    } else {
-        return next()
-    }
-})
-
-
-
-
-
-module.exports = mongoose.model("User", UserSchema)
+module.exports = mongoose.model("User", UserSchema);
